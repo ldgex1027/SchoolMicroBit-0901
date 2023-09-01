38920c1e8675d6ef884e4faed69a1fe19843763f
Lee Maneon <support@pcor.me>
2023-09-01 09:26:51 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,64 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (blinkLED == 1) {
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.CYAN)
+        blinkLED = 0
+    } else {
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+        blinkLED = 1
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+    } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 50)
+    } else if (receivedNumber == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+    } else if (receivedNumber == 4) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            `)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
+    }
+})
+let blinkLED = 0
+DFRobotMaqueenPlus.I2CInit()
+radio.setGroup(19)
+DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.PINK)
+basic.showString("WELCOME")
+basic.forever(function () {
+	
+})
